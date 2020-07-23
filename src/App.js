637845bcf5265cb6ac9f12a3a8736980899ba2c5
KONNO kiyotaka <konnokiyotaka@gmail.com>
2020-07-24 08:48:18 +0900

    create class and funtional component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return     <h1>Hello World</h1>;
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </React.Fragment>
+    )
+  }
 }
+// function App() {
+//   return     <h1>Hello World</h1>;
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Cat />
+//       <Cat />
+//       <Cat />
+//     </div>
+//   )
+// }
+
+// const Cat = () => {
+//   return <div>Meow!</div>
+// }
 
 export default App;

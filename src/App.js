b0637459c15cc4552e53b6350e826dedd4083776
KONNO kiyotaka <konnokiyotaka@gmail.com>
2020-07-24 09:56:14 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked!")}} />
-      </React.Fragment>
-    )
-  }
+const App = () => {
+  const profiles = [
+    {name: "taro", age: 10},
+    {name: "hanako", age: 5 },
+    {name: "noname", }
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        } )
+      }
+
+    </div>
+  )
 }
-// function App() {
-//   return     <h1>Hello World</h1>;
-// }
 
-// const App = () => {
-//   return (
-//     <div>
-//       <Cat />
-//       <Cat />
-//       <Cat />
-//     </div>
-//   )
-// }
+const User = (props) => {
+return <div>I am {props.name}, and {props.age} years old !</div>
+}
 
-// const Cat = () => {
-//   return <div>Meow!</div>
-// }
+User.defaultProps = {
+  age: 1
+}
 
 export default App;
